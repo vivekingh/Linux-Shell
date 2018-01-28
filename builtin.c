e2451438656d8bcae55a1b680d439b8725cffa6e
VIVEK SINGH <cool.vivekiitian@gmail.com>
2018-01-28 17:31:59 +0530

    Add files via upload

--- builtin.c
+++ builtin.c
@@ -1,6 +1,13 @@
 void endit(){
 	exit(-1);
 }
+
+void resetbg(bgprocess bg[],int x){
+	bg[x].status = -1;
+	bg[x].pd = -1;
+	strcpy(bg[x].cmd,"\0");
+}
+
 int isbuiltin(char *a){
 	if(strcmp(a,"cd")==0)
 		return 0;
@@ -15,8 +22,9 @@ int isbuiltin(char *a){
 	return -1;
 }
 
-void runbuiltin(int c,char **args){
+void runbuiltin(int c,char **args,int nofc,bgprocess bg[]){
 
+	int x = 0;
 	switch(c){
 
 		case 0:
@@ -24,6 +32,28 @@ void runbuiltin(int c,char **args){
 			break;
 
 		case 1:
+			for(x=1;x<arlen;x++){
+				if(bg[x].status==STATUS_DONE){
+					printf("[%d]+  Done\t\t%s\n",x, bg[x].cmd);
+					resetbg(bg,x);
+				}
+				else if(bg[x].status==STATUS_TER){
+					printf("[%d]+  Ter\t\t%s\n",x, bg[x].cmd);
+					resetbg(bg,x);
+				}
+				else if(bg[x].status==STATUS_RUN){
+					if(kill(bg[x].pd,0)<0){
+							bg[x].status = STATUS_DONE;
+							printf("[%d]+  Done\t\t%s\n",x, bg[x].cmd);
+							resetbg(bg,x);
+					}
+					else{
+						printf("[%d]+  Run\t\t%s\n", x,bg[x].cmd);
+					}
+					//printf("hello");
+				}
+
+			}
 			break;
 
 		case 2:
@@ -31,6 +61,29 @@ void runbuiltin(int c,char **args){
 			break;
 
 		case 3:
+			;
+			int x = atoi(args[nofc-1]);
+			if(x>0){
+				if(x>arlen-1){
+					printf("Incorrect Entry\n");
+				}
+				else{
+					if(bg[x].status==-1 || bg[x].status==STATUS_DONE || bg[x].status==STATUS_TER){
+						printf("Process doen't exist\n");
+					}
+					else{
+						if(kill(bg[x].pd,0)<0){
+							bg[x].status = STATUS_DONE;
+							printf("Process has already completed\n");
+						}
+						else{
+							kill(bg[x].pd,SIGKILL);
+							bg[x].status = STATUS_TER;
+						}
+						size--;
+					}
+				}
+			}
 			break;
 
 		case 4:
@@ -38,9 +91,9 @@ void runbuiltin(int c,char **args){
 			register HIST_ENTRY **the_list;
  			register int i;
  			the_list = history_list ();
-          		if (the_list)
-            		for (i = 0; the_list[i]; i++)
-              			printf ("%d: %s\n", i + history_base, the_list[i]->line);
+          	if (the_list)
+            for (i = 0; the_list[i]; i++)
+              printf ("%d: %s\n", i + history_base, the_list[i]->line);
 			break;
 
 	}
