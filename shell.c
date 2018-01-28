a9e68c01bd7310a719e9ba3956f1c7d3ff87cf6d
VIVEK SINGH <cool.vivekiitian@gmail.com>
2018-01-28 17:32:51 +0530

    Add files via upload

--- shell.c
+++ shell.c
@@ -10,10 +10,37 @@
 #include <limits.h>
 #include <time.h>
 
-#include "builtin.c"
+
 
 #define len 500
+#define arlen 50
+#define STATUS_TER 0
+#define STATUS_RUN 1
+#define STATUS_DONE 2
 
+
+
+typedef struct bgprocess{
+	int status;
+	int pd;
+	char cmd[len];
+}bgprocess;
+
+int size = 0;
+
+#include "builtin.c"
+
+
+
+
+void init(bgprocess bg[],int length){
+	int i = 0;
+	for(i=0;i<length;i++){
+		bg[i].status = -1;
+		bg[i].pd = -1;
+		bg[i].cmd[0] = '\0';
+	}
+}
 
 char *printpromt(){
 
@@ -21,7 +48,7 @@ char *printpromt(){
 	char *d = (char *)malloc(len*sizeof(char));
 	getcwd(a,len);//directory path
 	gethostname(b,len);
-    	getlogin_r(c,len);
+    getlogin_r(c,len);
 	snprintf(d,len,"%s@%s:~%s$ ", c,b,a);
 	return d;
 
@@ -50,17 +77,29 @@ int parsecommand(char *l,char **args){
 
 }
 
+
 int main(int argc, char const *argv[])
 {
 
-	char a[len];
-	int pid,stat;
+	char a[len],b[len],e[len];
+	int pid,stat,bckjob = 0;
+	bgprocess bg[arlen];
+	init(bg,arlen);
+
+	signal(SIGINT, SIG_IGN);//ignoring ctrl+c
 
 	while(1){
+
+		here:
+
+		bckjob = size==0?size:bckjob;
 
 		char *p = printpromt();
 
 		char *l = readline(p);//reading command
+
+		strcpy(b,l);
+
 		free(p);
 
 		char* args[50] = {NULL};
@@ -71,34 +110,54 @@ int main(int argc, char const *argv[])
 		strcpy(a,l);
 		recordhistory(a,&l);
 
+
 		//printf("%s\n",l );
 		int nofc = parsecommand(l,args);//command parsing
+
+
 
 		if(nofc==0)
 			continue;
 
+
 		//for background process
 		if(strcmp(args[nofc-1],"&")==0){
+
 			pid = fork();
+
 			if(pid<0){
 				perror("Fork Error: Process couldn't be created\n");
+				exit(-1);
 			}
+
 			if(pid==0){
 				args[nofc-1] = NULL;
 				execvp(*args,args);
 				exit(-1);
 			}
+	
 			waitpid(-1,&stat, WNOHANG);
+
+			if(bckjob+1<arlen){
+
+				printf("[%d] %d\n",++bckjob,pid);
+				size++;
+				bg[bckjob].status = STATUS_RUN;
+				bg[bckjob].pd = pid;
+				strcpy(bg[bckjob].cmd,b);
+
+			}
 			
-			//printf("hello\n");
 			continue;
 		}
+
+
 
 		int check = isbuiltin(args[0]);
 
 
 		if(check>=0){
-			runbuiltin(check,args);
+			runbuiltin(check,args,nofc,bg);
 		}
 		else{
 
@@ -107,9 +166,23 @@ int main(int argc, char const *argv[])
 				perror("Fork Error: Process couldn't be created\n");
 			}
 			if(pid==0){
+
+				if(strcmp(args[0]+strlen(args[0])-2,".c")==0){
+					strcpy(e,"gcc ");
+					strcat(e,args[0]);
+					system(e);
+					strcpy(e,"./a.out ");
+					strcat(e,args[1]);
+					strcat(e," ");
+					strcat(e,args[2]);
+					system(e);
+					exit(-1);
+				}
+
 				execvp(*args,args);
 				system(l);
 				exit(-1);
+
 			}
 			else{
 				wait(NULL);
