a9ede363adcfc9b9ecdb07da1c7855802f0b414c
VIVEK SINGH <cool.vivekiitian@gmail.com>
2018-01-28 11:48:49 +0530

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -38,9 +38,9 @@ void runbuiltin(int c,char **args){
 			register HIST_ENTRY **the_list;
  			register int i;
  			the_list = history_list ();
-          	if (the_list)
-            for (i = 0; the_list[i]; i++)
-              printf ("%d: %s\n", i + history_base, the_list[i]->line);
+          		if (the_list)
+            		for (i = 0; the_list[i]; i++)
+              			printf ("%d: %s\n", i + history_base, the_list[i]->line);
 			break;
 
 	}
